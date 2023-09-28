0293014cdbc7cdfa7e62235f680405712c00c2d2
MirageM <imiragesz@gmail.com>
2023-09-27 23:23:48 -0400

    next = current.next = prev = current = next

---
+++ reverseListRedo.java
@@ -0,0 +1,18 @@
+public class reverseListRedo{
+	public ListNode reverseList(ListNode head){
+		if head == null{
+			return null;
+		}
+		ListNode current = head;
+		ListNode prev = null;
+		ListNode next = null;
+
+		while(current != null){
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		return prev;
+	}
+}
